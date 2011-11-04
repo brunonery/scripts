306b997522442bab8b8136e0e095e91e8eec6583
Bruno Nery <brunonery@brunonery.com>
2011-11-04 12:48:06 +0000

    Implemented script to merge two point clouds in .ply format.

---
+++ merge_ply_point_clouds.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) == 3:
+        first_file = open(argv[1], 'r')
+        first_lines = first_file.readlines()
+        first_file.close()
+        second_file = open(argv[2], 'r')
+        second_lines = second_file.readlines()
+        second_file.close()
+        first_count = int(first_lines[2].split()[2])
+        second_count = int(second_lines[2].split()[2])
+        print 'ply'
+        print 'format ascii 1.0'
+        print 'element vertex %d' % (first_count + second_count)
+        print 'property float x'
+        print 'property float y'
+        print 'property float z'
+        print 'end_header'
+        for l in first_lines[7:]:
+            print l.strip()
+        for l in second_lines[7:]:
+            print l.strip()
